885952de462c836357e554d3b190648126017fa8
parkvsmin <sdfjkl658@naver.com>
2022-08-18 09:03:28 +0900

    11

--- src/main/java/com/iu/home/bankMembers/BankMembersDAO.java
+++ src/main/java/com/iu/home/bankMembers/BankMembersDAO.java
@@ -1,26 +1,34 @@
 package com.iu.home.bankMembers;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BankMembersDAO implements MembersDAO {
-	
+
 	@Autowired
 	private SqlSession sqlSession;
-	
+
 	private final String NAMESPACE="com.iu.home.bankMembers.BankMembersDAO";
-	
+
 	public BankMembersDTO getLogin(BankMembersDTO bankMembersDTO)throws Exception{
-		
+
 		return sqlSession.selectOne(NAMESPACE+"getLogin", bankMembersDTO);		
 	}
-	
+
 	public int setJoin(BankMembersDTO bankMembersDTO)throws Exception{
 		// TODO Auto-generated method stub
 		return sqlSession.insert(NAMESPACE+"setJoin", bankMembersDTO);
 	}
-	
-	
+
+	@Override
+	public List<BankMembersDTO> getSearchByID(String search) throws Exception {
+		// TODO Auto-generated method stub
+		return sqlSession.selectList(NAMESPACE+"search");
+	}
+
+
 }
