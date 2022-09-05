5aa7c12f65e98b7d1797173df6f68cbd17246b9a
parkvsmin <sdfjkl658@naver.com>
2022-09-05 23:00:33 +0900

    0905

--- src/main/java/com/iu/home/bankBook/CommentDAO.java
+++ src/main/java/com/iu/home/bankBook/CommentDAO.java
@@ -1,5 +1,7 @@
 package com.iu.home.bankBook;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 public class CommentDAO {
@@ -7,7 +9,21 @@ public class CommentDAO {
 	private SqlSession sqlSession;
 	private final String NAMESPACE = "com.iu.home.bankBook.CommentDAO.";
 	
+	public int commentAdd(BankBookCommentDTO bankBookCommentDTO)throws Exception{
+		return sqlSession.insert(NAMESPACE+"commentAdd",bankBookCommentDTO);
+	}
+	
+	public List<BankBookCommentDTO> commentList()throws Exception {
+		return sqlSession.selectList(NAMESPACE+"commentList");
+	}
+	
+	public int commentUpdate(BankBookCommentDTO bankBookCommentDTO)throws Exception {
+		return sqlSession.update(NAMESPACE+"commentUpdate", bankBookCommentDTO);
+	}
+	
+	public int commentDelete(BankBookCommentDTO bankBookCommentDTO)throws Exception {
+		return sqlSession.delete(NAMESPACE+"commentDelete", bankBookCommentDTO);
+	}
 	
 	
-
 }
